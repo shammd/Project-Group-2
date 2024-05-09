f959ab4cfadd260ad99e13e051c48a4c8ea82d6d
shammd <160494380+shammd@users.noreply.github.com>
2024-05-09 23:07:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,219 +12,297 @@ https://education.lego.com/en-us/support/mindstorms-ev3/building-instructions#bu
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
-from pybricks.parameters import Port, Stop, Direction, Color
+from pybricks.parameters import Port, Stop, Direction, Color, Button
 from pybricks.tools import wait
-import time 
 
-# Initialize the EV3 Brick
 ev3 = EV3Brick()
 
-# Configure the gripper motor on Port A with default settings.
 gripper_motor = Motor(Port.A)
 
-# Configure the elbow motor. It has an 8-teeth and a 40-teeth gear
-# connected to it. We would like positive speed values to make the
-# arm go upward. This corresponds to counterclockwise rotation
-# of the motor.
-elbow_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE, [8, 40])
+arm_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE, [8, 40])
 
-# Configure the motor that rotates the base. It has a 12-teeth and a
-# 36-teeth gear connected to it. We would like positive speed values
-# to make the arm go away from the Touch Sensor. This corresponds
-# to counterclockwise rotation of the motor.
 base_motor= Motor(Port.C, Direction.COUNTERCLOCKWISE, [12,36])
 
+MAX_SPEED = 200
+MAX_ACCELERATION = 120
+arm_motor.control.limits(MAX_SPEED, MAX_ACCELERATION)
+base_motor.control.limits(MAX_SPEED, MAX_ACCELERATION)
 
-# Limit the elbow and base accelerations. This results in
-# very smooth motion. Like an industrial robot.
-elbow_motor.control.limits(speed=60, acceleration=120)
-base_motor.control.limits(speed=60, acceleration=120)
-
-# Set up the Touch Sensor. It acts as an end-switch in the base
-# of the robot arm. It defines the starting point of the base.
 base_switch = TouchSensor(Port.S1)
 
-# Set up the Color Sensor. This sensor detects when the elbow
-# is in the starting position. This is when the sensor sees the
-# white beam up close.
 color_sensor = ColorSensor(Port.S2)
 
 """ Code starts here """
 
-# Define förinställda positioner i grader relativt till zero point
-POSITIONS = {
-    1: -100, # 
-    2: 0,    # zero point
-    3: 47,   # ungefär 45° från andra position (135° från zero point) - drop off zone för gröna objekt
-    4: 90,   # ungefär 45° från tredje position (180° från zero point)
-    5: -51   # Extra postion sido led 
-} 
+# To difine positions for pick up, elevated and drop zone
+POSITIONS = {}
 
-COLOR_LST = [Color.RED, Color.GREEN, Color.YELLOW]
+# Level of arm to move
+ARM_LEVEL = {'down':0, 'ColorSensor':0, 'up':0}
 
-P1 = []
-P2 = []
-P3 = [Color.RED, Color.GREEN]
-P4 = [Color.YELLOW]
-P5 = []
+COLOR_LST = ['Red','Blue','Green','Yellow']
 
-# Function to close the gripper
+# To close the gripper
 def close_gripper():
-    gripper_motor.run_until_stalled(200, then=Stop.HOLD, duty_limit=35)
-    print('close')
+    gripper_motor.run_until_stalled(200, then=Stop.HOLD, duty_limit=50)
 
-# Function to open the gripper
+# To open the gripper
 def open_gripper():
-    gripper_motor.run_target(150, -90)
-    #gripper_motor.run_until_stalled(-200, then=Stop.HOLD, duty_limit=7)
-    print('open')
+    gripper_motor.run_target(150, 0)
 
-# Function to move the elbow upward
-def elbow_up(degree):
-    elbow_motor.run_angle(60,degree)
+# To reset base robot: Angle 0 = middel, positive angle = left side, negative angle = right side.
+def reset_robot():
+    #reset base to right side 
+    while not base_switch.pressed():
+        base_motor.run(-100)
+    #Set the position as angel zero
+    base_motor.reset_angle(0)
 
-# Function to move the elbow downward
-def elbow_down(degree):
-    elbow_motor.run_angle(60,-degree)
+# To reset gripper: We want to set open gripp as angel zero
+def reset_gripper():
+    robot_display('Reset gripper')
+    reset_gripper = False
+    while not reset_gripper:
+        move_angel = 0
+        #if center button is pressed
+        if Button.CENTER in ev3.buttons.pressed():
+            wait(500)
+            #set open gripp as angel zero
+            gripper_motor.reset_angle(0) 
+            #go out from while loop
+            reset_gripper = True
+            continue
+        elif Button.LEFT in ev3.buttons.pressed():
+            #rotate base to the left
+            wait(100)
+            move_angel = 10
+            gripper_motor.run_angle(MAX_SPEED,move_angel)
+        elif Button.RIGHT in ev3.buttons.pressed():
+            wait(100)
+            #rotate base to the left
+            move_angel = -10
+            gripper_motor.run_angle(MAX_SPEED,move_angel)
+        else: 
+            wait(100)
 
-# Funktion för att detektera färg
-def detect_color():
-    # Läs färgen från färgsensorn
-    detected_color = color_sensor.color()
-    print(detected_color)
-    return detected_color
+# To define level for arm robot and return angle for each level
+def set_arm():
+    set_arm = False
+    while not set_arm:
+        move_angel = 0
+        #if center button is pressed
+        if Button.CENTER in ev3.buttons.pressed():
+            wait(500)
+            set_arm = True
+            #return angle of arm
+            return arm_motor.angle()
+        elif Button.UP in ev3.buttons.pressed():
+            wait(100)
+            #move up the arm
+            move_angel = 5
+            arm_motor.run_angle(MAX_SPEED,move_angel)
+        elif Button.DOWN in ev3.buttons.pressed():
+            wait(100)
+            #move down the arm
+            move_angel = -5
+            arm_motor.run_angle(MAX_SPEED,move_angel)
+        else:
+            wait(100)
 
-# Funktion för att uttala färgens namn
-def announce_color(color):
-    if color == Color.RED:
-        ev3.speaker.say("Red")
-    elif color == Color.GREEN:
-        ev3.speaker.say("Green")
-    elif color == Color.YELLOW:
-        ev3.speaker.say("Yellow")
-    elif color == Color.BLUE:
-        ev3.speaker.say("Blue")
-    elif color == None:
-        ev3.speaker.say('No item found')
+# To set level of arm to move and set angel for each level
+def set_level():
+    #get into value for each level
+    for level in ARM_LEVEL.keys():
+    #get angle for each level
+        robot_display('Set for: ' + level)
+    #set angle for value for each level
+        angle = set_arm()
+        ARM_LEVEL[level] = angle
+        wait(200)
+        
+# To move arm to given level
+def move_arm(level):
+    #get target level
+    target_level = ARM_LEVEL.get(level)
+    #get current level
+    current_level = arm_motor.angle()
+    #get diff level
+    delta_level = target_level - current_level
+    #rotate arm with diff level to target level
+    arm_motor.run_angle(MAX_SPEED, delta_level) 
 
-            
+# To define position for base robot and return angle for each position
+def set_base():
+    set_base = False
+    while not set_base:
+        move_angel = 0
+        #if center button is pressed
+        if Button.CENTER in ev3.buttons.pressed():
+            wait(500)
+            set_base = True
+            #return angle of base
+            return base_motor.angle()
+        elif Button.LEFT in ev3.buttons.pressed():
+            wait(100)
+            #rotate base to the left
+            move_angel = 10
+            base_motor.run_angle(MAX_SPEED,move_angel)
+        elif Button.RIGHT in ev3.buttons.pressed():
+            wait(100)
+            #rotate base to the left
+            move_angel = -10
+            base_motor.run_angle(MAX_SPEED,move_angel)
+        else: 
+            wait(100)
+
+# To get number to use in another function
+def get_number(step=1):
+    number = 0
+    while True:
+        robot_display(str(number))
+        #if center button is pressed
+        if Button.CENTER in ev3.buttons.pressed():
+            wait(500)
+            return number
+        elif Button.UP in ev3.buttons.pressed():
+            wait(100)
+            #number plus one
+            number += step
+        elif Button.DOWN in ev3.buttons.pressed():
+            wait(100)
+            #number minus one
+            number -= step
+        wait(200)
+
+# To set position and zone for base robot  
+def set_positions():
+    #set position amount
+    robot_display('Total positions')
+    wait(1500)
+    positions = get_number()
+    #define angle for all positions
+    for number in range(positions):
+        robot_display('Set position:'+ str(number+1))
+        wait(1500)
+        #get angle from buttons to dictionary
+        POSITIONS[number+1] = set_base()
+        wait(200)
+
+    #set pick up position
+    robot_display('Pick up' '\n' 'position:')
+    wait(1500)
+    pick_up_position = get_number()
+    
+    #set drop position
+    robot_display('Drop position' '\n' 'for color:')
+    wait(1500)
+    drop_positions = {}
+    for color in COLOR_LST:
+        robot_display(color)
+        wait(1500)
+        number = get_number()
+        drop_positions[color] = number
+        wait(200)
+    
+    #return pick up and drop positions
+    return (pick_up_position, drop_positions)
+
+# To move base to given position        
 def item_zone(position):
     target_position = POSITIONS.get(position)
-    if target_position is None:
-        print("Ogiltig position!")
-        return
 
     current_angle = base_motor.angle()
     delta_angle = target_position - current_angle
 
-    # Roterar armen till den förinställda positionen
-    base_motor.run_angle(60, delta_angle)
+    base_motor.run_angle(MAX_SPEED, delta_angle)
 
-def elevated_position(position):
-    item_zone(position)
-    item_color = detect_color()
-    if item_color in COLOR_LST:
-        open_gripper()
-        elbow_down(55)
-        close_gripper()
-    return item_color
+# To detect the color of item and return item color as string
+def detect_color():
+    color = color_sensor.color()
+    if color == Color.RED:
+        color = "Red"
+    elif color == Color.GREEN:
+        color = "Green"
+    elif color == Color.YELLOW:
+        color = "Yellow"
+    elif color == Color.BLUE:
+        color = "Blue"
+    else:
+        color = 'Color not in list'
+    return color
 
-def reset_robot():
-    while not base_switch.pressed():
-        base_motor.run(-60)
-    base_motor.run_angle(60,110)
-    base_motor.reset_angle(0)   
+# To announce the color of item
+def announce_color(color):
+    ev3.speaker.say(color)
 
+# To print the msg on display
+def robot_display(msg):
+    ev3.screen.clear()
+    ev3.screen.print(msg)
+
+# To pick item and control if gripper has item or not 
+# If not robot gonna ask how long time need robot to wait before start pick item again 
 def pick_item(position):
-    item_zone(position) 
-    elbow_down(90)
-    close_gripper()
-    elbow_up(45)
-
-def drop_item(position):
-    elbow_up(45)
+    picked = False
     item_zone(position)
-    elbow_down(90)
-    open_gripper()
-    elbow_up(90)
-
-def sort_item_by_time():
-    pick_item(1)
-    item_color = detect_color()
-    if item_color in COLOR_LST:
-        drop_item(3)
-    else: reset_robot()
-
-def sort_objects_for_time(duration):
-    start_time = time.time()  # Hämta starttiden
-    while time.time() - start_time < duration:  # Utför sortering under angiven tid
-        ev3.speaker.say('Start')
-
-def NoItemFound():
-    elbow_up(45) 
-    open_gripper()
-    
-def reset_arm(open_, up_90):
-    if open_ == 0:
-        open_gripper()
-    elif open_ == 1:
-        pass
-    elif open_ == 2:
+    while not picked: 
+        move_arm('down')
         close_gripper()
-    
-    if up_90 == 90:
-        pass
-    elif up_90 == 0:
-        elbow_up(90)
+        # if gripper = 92 => no item
+        current_angle = gripper_motor.angle()
+        close_angle = 80
+        if current_angle >= close_angle:
+            # No item 
+            open_gripper()
+            move_arm('up') 
+            #ask time for wait
+            robot_display('Number for' '\n' 'wait time(ms)?')
+            wait(1500)
+            #input time by button
+            wait_time = get_number(step=1000)
+            #wait
+            wait(wait_time)
+            #pick again
+        else: 
+            # Have item
+            move_arm('ColorSensor')
+            picked = True
 
+# To drop item at given zone
+def drop_item(position):
+    move_arm('up')
+    item_zone(position)
+    move_arm('down')
+    open_gripper()
+    move_arm('up')
 
 def main():
-    open_gripper()
-    elbow_up(90)
-    reset_robot()
+    robot_display('Start set up')
+    wait(1500)
+    reset_gripper()
+    set_level() 
+    pick_up_position, drop_positions = set_positions()
+    robot_display('Finish set up')
     while True:
-        pick_item(1)
-        item_C = detect_color()
-        announce_color(item_C)
-        if item_C == Color.RED:
-            drop_item(3)
-        elif item_C == Color.GREEN: 
-            drop_item(3)
-        elif item_C == Color.YELLOW:
-            drop_item(4)
-        else: 
-            NoItemFound()
-            continue
-
-def R_main():
-    reset_arm()
-    reset_robot()
-    L_P = False 
-    while not L_P:
-        count = 0
-        pick_item(1)
-        item_C = detect_color()
-        announce_color(item_C)
-        if item_C in P3:
-            drop_item(3)
-        elif item_C in P4:
-            drop_item(4)
-        elif item not in COLOR_LST: 
-            NoItemFound()
-            count += 1
-        itemC = elevated_position(2)
-        announce_color(itemC)
-        if itemC in P3:
-            drop_item(3)
-        elif itemC in P4:
-            drop_item(4)
-        elif itemC not in COLOR_LST: 
-            continue
-            count += 1
+        pick_item(pick_up_position)
+        item_color = detect_color()
+        ev3.speaker.say(item_color)
         
-        if count == 20:
-            L_P = True
+        #Get drop position if color in color list
+        if item_color in drop_positions.keys():
+            #get drop position from color list
+            drop_position = drop_positions.get(item_color)
+        elif item_color not in drop_positions.keys():
+            #if not color in color list
+            #ask number for position
+            robot_display('Where do you want' '\n' 'to drop the item?')
+            wait(1500)
+            #get drop position from buttons
+            drop_position = get_number()
+            
+        #drop item at the drop position
+        drop_item(drop_position)
 
 if __name__ == "__main__":
     main()
-    
+
